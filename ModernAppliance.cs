acc533b247cd1c31468f93d5fec9073e1c46006d
jooyoung <yongin016@gmail.com>
2024-06-26 02:03:20 -0600

    Check out method(without decreasing the num of quantity)

--- ModernAppliance.cs
+++ ModernAppliance.cs
@@ -4,9 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP2GroupLab1
+namespace OOP2GroupLab1.ProblemDomain
 {
-    internal class ModernAppliance
+    internal class ModernAppliance : Appliance
     {
+        public override void Checkout()
+        {
+            TextToList getList = new TextToList();
+            List<string[]> appliance_list1 = getList.TextIterator();
+
+            List<string> itemNumber = new List<string>();
+
+            foreach (string[] s in appliance_list1)
+            {
+                itemNumber.Add(s[0]);
+            }
+            string searchNumber = Console.ReadLine();
+
+
+            if (itemNumber.Contains(searchNumber) == false)
+            {
+                Console.WriteLine("\nNo appliances found with that item number.");
+            }
+            else if (itemNumber.Contains(searchNumber))
+            {
+                for (int i = 0; i < appliance_list1.Count; i++)
+                {
+
+                    if (int.Parse(appliance_list1[i][0]) == int.Parse(searchNumber))
+                    {
+                        if (IsAvailable() == true)
+                        {
+                            Console.WriteLine($"\nAppliance \"{searchNumber}\" has been checked out.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"\nThe appliance \"{searchNumber}\" is not available to be checked out.");
+                        }
+                    }
+                }
+
+            }
+
+        }
     }
 }
